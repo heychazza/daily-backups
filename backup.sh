874502cbae6cd8266e97cc6ce2f87bb730a16e81
heychazza <ch@rli.es>
2021-08-21 17:22:57 +0100

    Added backup script

---
+++ backup.sh
@@ -0,0 +1,38 @@
+# Read environment variables.
+set -o allexport; source .env; set +o allexport
+
+# Backup storage directory
+backupFolder=storage
+
+# MySQL user
+user=$DB_USERNAME
+# MySQL password
+password=$DB_PASSWORD
+
+# Number of days to store the backup
+keep_day=30
+
+fileName=$(date +%d-%m-%Y_%H-%M-%S)
+sqlfile=all-database-$fileName.sql
+zipfile=$backupFolder/all-database-$fileName.zip
+
+# Create a backup
+mysqldump -u $user -p$password --all-databases > $sqlfile
+if [ $? == 0 ]; then
+  echo '[BACKUP] Successfully created SQL dump.'
+else
+  echo '[BACKUP] Failed to perform SQL dump.'
+  exit
+fi
+# Compress backup
+zip $zipfile $sqlfile
+if [ $? == 0 ]; then
+  echo '[BACKUP] Successfully compressed SQL dump.'
+else
+    echo '[BACKUP] Failed to compress SQL dump.'
+    exit
+fi
+    rm $sqlfile
+    echo $zipfile
+    # Delete old backups
+    find $backupfolder -mtime +$keep_day -delete
